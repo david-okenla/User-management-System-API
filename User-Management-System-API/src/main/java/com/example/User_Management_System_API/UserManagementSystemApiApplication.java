0c49f22577c41049c6445c282e11f5cf4d38d1fb
David Okenla <dvdokenla@gmail.com>
2026-02-26 20:51:34 +0100

    First push!

---
+++ User-Management-System-API/src/main/java/com/example/User_Management_System_API/UserManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.User_Management_System_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementSystemApiApplication.class, args);
+	}
+
+}
---
+++ User-Management-System-API/src/test/java/com/example/User_Management_System_API/UserManagementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.User_Management_System_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
